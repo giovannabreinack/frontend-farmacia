862276289dd50b85ff1ce253e3b13cface30bb19
Gih <giovannabreinackcolombara@gmail.com>
2025-08-05 12:56:42 -0300

    adicionando navbar, footer e home

---
+++ farmacia/src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-linear-to-r/srgb from-teal-500 to-teal-700
+                            text-white font-raleway'>
+
+                <div className="container flex justify-between text-lg">
+                    RemédiAqui
+
+                    <div className='flex gap-4'>
+                        Categorias
+                        Cadastrar categoria
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ farmacia/src/pages/home/Home.tsx
@@ -0,0 +1,21 @@
+function Home() {
+    return (
+        <>
+            <div className="w-screen flex justify-center">
+                <div className="max-w-7xl flex flex-col items-center font-raleway ">
+                    <h2>RemédiAqui</h2>
+                    <p>O clique que cura!</p>
+                </div>
+                <div className="max-w-7xl flex flex-col items-center">
+                    <img
+                        src="https://ik.imagekit.io/zddqh4rhi/ilustracao-plana-do-dia-do-medico-nacional-com-medico-e-caixa-de-primeiros-socorros.png?updatedAt=1754405822422"
+                        alt="Imagem da Página Home"
+                        width="400px"
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
